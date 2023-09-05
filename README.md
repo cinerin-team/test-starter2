a044d8b2331969cf0d6dce8819c997b6b4f39d69
Miklos Parrag <miklos.parrag@ericsson.com>
2023-09-05 18:54:06 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_starter2
+
+https://tutswiki.com/read-write-yaml-config-file-in-python/
---
+++ runner.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+path = "configs"
+dir_list = os.listdir(path)
+conf = {}
+
+
+def create_command(param):
+    result = "autott"
+    if "build" in param.keys():
+        result = result + " --build-id " + param["build"]
+    if "tc" in param.keys():
+        result = result + " --test-case \"" + param["tc"] + "\""
+    if "constraints" in param.keys():
+        result = result + " " + param["constraints"]
+    if "node_type" in param.keys():
+        result = result + " --node-type " + param["node_type"]
+
+    return result
+
+
+for file in dir_list:
+    conf.clear()
+    f = open(path + "/" + file, "r")
+    for line in f.readlines():
+        conf[line.split(": ")[0]] = line.split(": ")[1].rstrip("\n")
+
+    output = subprocess.Popen(["~/epg/queue_run2.py", create_command(conf)],
+                              stdout=subprocess.PIPE)
+    out, err = output.communicate()
+    print(out)
+    print(err)
