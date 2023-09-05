f93ccd29a859f53e9d35b591fcb92b2fb114e28e
Miklos Parrag <miklos.parrag@ericsson.com>
2023-09-05 19:12:55 +0200

    fix

--- runner.py
+++ runner.py
@@ -10,10 +10,8 @@ def create_command(param):
     result = "autott"
     if "build" in param.keys():
         result = result + " --build-id " + param["build"]
-    if "tc" in param.keys():
-        result = result + " --test-case \"" + param["tc"] + "\""
-    if "constraints" in param.keys():
-        result = result + " " + param["constraints"]
+    if "tc" in param.keys() and "constraints" in param.keys():
+        result = result + " --test-case \"" + param["tc"] + " " + param["constraints"] + "\""
     if "node_type" in param.keys():
         result = result + " --node-type " + param["node_type"]
 
@@ -26,7 +24,7 @@ for file in dir_list:
     for line in f.readlines():
         conf[line.split(": ")[0]] = line.split(": ")[1].rstrip("\n")
 
-    output = subprocess.Popen(["~/epg/queue_run2.py", create_command(conf)],
+    output = subprocess.Popen(["queue_run2.py", create_command(conf)],
                               stdout=subprocess.PIPE)
     out, err = output.communicate()
     print(out)
