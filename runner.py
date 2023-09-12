0442b2ab77616596361d470ea016cba364be6d52
Miklos Parrag <miklos.parrag@ericsson.com>
2023-09-12 09:56:34 +0200

    added commandline parameter and template config

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+import sys
 from datetime import datetime
 
 path = "configs"
@@ -9,10 +10,13 @@ conf = {}
 job_ids = []
 
 
-def create_command(param):
+def create_command(param, args):
     result = "autott"
-    if "build" in param.keys():
-        result = result + " --build-id " + param["build"]
+    if "build" in param.keys() or len(args) == 2:
+        if len(args) == 2:
+            result = result + " --build-id " + args[1]
+        else:
+            result = result + " --build-id " + param["build"]
     if "tc" in param.keys() and "command" in param.keys():
         result = result + " --test-case \"" + param["tc"] + " : " + param["command"] + "\""
     if "node_type" in param.keys():
@@ -21,6 +25,12 @@ def create_command(param):
         result = result + " --resource " + param["resource"]
     if "model" in param.keys():
         result = result + " --constraints \'[(model == \"" + param["model"] + "\")]\'"
+    if "epg_path" in param.keys():
+        result = result + " --epgcats-path \'" + param["epg_path"] + "\'"
+    if "dallas-path" in param.keys():
+        result = result + " --dallas-path \'" + param["dallas-path"] + "\'"
+    if "autott-path" in param.keys():
+        result = result + " --autott-path \'" + param["autott-path"] + "\'"
 
     return result
 
@@ -38,8 +48,8 @@ for file in dir_list:
         f = open(path + "/" + file, "r")
         for line in f.readlines():
             conf[line.split(": ")[0]] = line.split(": ")[1].rstrip("\n")
-        print("queue_run2.py " + create_command(conf))
-        output = subprocess.Popen(["queue_run2.py " + create_command(conf)],
+        print("queue_run2.py " + create_command(conf, sys.argv))
+        output = subprocess.Popen(["queue_run2.py " + create_command(conf, sys.argv)],
                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         out, err = output.communicate()
         print(str(out))
@@ -47,4 +57,4 @@ for file in dir_list:
         # if mo:
         #     job_ids.append(mo.group(1))
 
-write_out_file(job_ids)
+# write_out_file(job_ids)
